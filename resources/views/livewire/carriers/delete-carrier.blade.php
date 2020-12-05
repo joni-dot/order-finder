15896d6f4ee819c0b70d632f7ecd6308862927e3
vagrant <vagrant@homestead>
2020-12-05 16:49:48 +0000

    Create Component for Livewire delete button

--- resources/views/livewire/carriers/delete-carrier.blade.php
+++ resources/views/livewire/carriers/delete-carrier.blade.php
@@ -1,4 +1,1 @@
-<button wire:click="delete()" class="h-10 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border rounded">
-    @svg('trash', 'fill-current w-4 h-4 mr-1 ml-1 inline')
-    <span class="mr-2">Delete</span>
-</button>
+<x-buttons.livewire.delete/>
