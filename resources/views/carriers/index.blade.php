2fa65412d2cfe42b02303615fede3be0d932efd6
samulimarkus <minjonlu@gmail.com>
2020-12-05 14:16:58 +0200

    Improve carrier listing button styles

--- resources/views/carriers/index.blade.php
+++ resources/views/carriers/index.blade.php
@@ -4,6 +4,7 @@
 
     <div class="text-right">
         <a href="/carriers/create" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            @svg('add-solid', 'fill-current w-4 h-4 mr-1 ml-0 inline')
             New carrier
         </a>
     </div>
@@ -11,19 +12,20 @@
         <table class="table-auto w-full">
             <thead>
                 <tr>
-                    <th class="text-right">ID</th>
-                    <th class="text-right">Name</th>
-                    <th class="text-right"></th>
+                    <th class="text-right pb-2 pt-2">ID</th>
+                    <th class="text-right pb-2 pt-2">Name</th>
+                    <th class="text-right pb-2 pt-2"></th>
                 </tr>
             </thead>
             <tbody>
                 @forelse($carriers as $carrier)
-                    <tr>
+                    <tr class="border-b border-t">
                         <td class="mb-2 pt-2 pb-2 text-right">{{ $carrier->id }}</td>
                         <td class="mb-2 pt-2 pb-2 text-right">{{ $carrier->name }}</td>
                         <td class="mb-2 pt-2 pb-2 text-right">
-                            <a href="/carriers/{{ $carrier->id }}/edit" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-2 border rounded">
-                                @svg('edit-pencil', 'fill-current w-4 h-4 mr-2 inline') Edit 
+                            <a href="/carriers/{{ $carrier->id }}/edit" class="h-10 inline-block bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-2 border rounded">
+                                @svg('edit-pencil', 'fill-current w-4 h-4 mr-1 ml-1 inline') 
+                                <span class="mr-2">Edit</span> 
                             </a>
                             <livewire:carriers.delete-carrier :carrier="$carrier">
                         </td>
