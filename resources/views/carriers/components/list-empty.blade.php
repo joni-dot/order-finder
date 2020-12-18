c18bd05a6471ce09000d0508b1d208955393b148
samulimarkus <minjonlu@gmail.com>
2020-12-18 19:23:01 +0200

    Move info alert to component

--- resources/views/carriers/components/list-empty.blade.php
+++ resources/views/carriers/components/list-empty.blade.php
@@ -1,11 +1,1 @@
-<div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-teal-500">
-    <span class="text-xl inline-block mr-5 align-middle">
-        @svg('information-outline', 'fill-current w-4 h-4 mr-1 ml-0 inline')
-    </span>
-    <span class="inline-block align-middle mr-8">
-        <b class="capitalize">Empty!</b> Cannot find any carriers.
-    </span>
-    <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
-        <span>×</span>
-    </button>
-</div>
+<x-alerts.info :highlight="'Empty!'" :message="'Cannot find any carriers.'"/>
