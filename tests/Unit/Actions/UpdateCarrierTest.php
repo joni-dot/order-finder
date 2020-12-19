d7570affb231e6a2f84ad9d832bcd3fcfab36ae0
samulimarkus <minjonlu@gmail.com>
2020-12-19 21:58:00 +0200

    Unit test for UpdateCarrier - action

---
+++ tests/Unit/Actions/UpdateCarrierTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+use App\Actions\Carriers\UpdateCarrier;
+use App\Carrier;
+
+class UpdateCarrierTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test that UpdateCarrierAction can create carrier.
+     *
+     * @return void
+     */
+    public function testThatItCanUpdateCarrier(): void
+    {
+        $existingCarrier = Carrier::create([
+            'name' => 'Old name',
+        ]);
+
+        (new UpdateCarrier)->execute($existingCarrier, [
+            'name' => 'New name',
+        ]);
+
+        $this->assertDatabaseCount('carriers', 1);
+
+        $this->assertDatabaseHas('carriers', [
+            'id' => $existingCarrier->id,
+            'name' => 'New name',
+        ]);
+    }
+}
