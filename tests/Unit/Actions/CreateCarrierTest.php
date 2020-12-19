d5b19c7927aff1dc8db98b7a909661b74cfe1f51
samulimarkus <minjonlu@gmail.com>
2020-12-19 13:43:19 +0200

    Unit test for CreateCarrier - action

---
+++ tests/Unit/Actions/CreateCarrierTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+use App\Actions\Carriers\CreateCarrier;
+
+class CreateCarrierTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test that CreateCarrierAction can create carrier.
+     *
+     * @return void
+     */
+    public function testThatItCanCreateCarrier(): void
+    {
+        (new CreateCarrier)->execute([
+            'name' => 'Carrier name',
+        ]);
+
+        $this->assertDatabaseCount('carriers', 1);
+
+        $this->assertDatabaseHas('carriers', [
+            'id' => 1,
+            'name' => 'Carrier name',
+        ]);
+    }
+}
