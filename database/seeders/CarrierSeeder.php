082bf694c45e7fe6328920bdef01ad1ab1fc86a5
samulimarkus <minjonlu@gmail.com>
2020-12-20 17:04:01 +0200

    Create CarrierSeeder so we cant make test data more easily

---
+++ database/seeders/CarrierSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Carrier;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CarrierSeeder extends Seeder
+{
+    /**
+     * Run the carrier seeders.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Carrier::create([
+            'name' => 'Posti',
+        ]);
+
+        Carrier::create([
+            'name' => 'Matkahuolto',
+        ]);
+
+        Carrier::create([
+            'name' => 'DHL',
+        ]);
+
+        Carrier::create([
+            'name' => 'DB Schenker',
+        ]);
+    }
+}
