d04942b34818c4e34601bc943a25e279a8f78073
samulimarkus <minjonlu@gmail.com>
2020-12-26 19:15:26 +0200

    Navigation tabs for carrier sub pages

--- resources/views/carriers/edit.blade.php
+++ resources/views/carriers/edit.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+            <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white bg-pink-600" onclick="changeAtiveTab(event,'tab-profile')">
+                Information
+            </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+            <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-pink-600 bg-white" onclick="changeAtiveTab(event,'tab-settings')">
+                Mode of transports
+            </a>
+        </li>
+    </ul>
     <livewire:carriers.edit-carrier :carrier="$carrier">
 @endsection
