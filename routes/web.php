f3925f532acfdfa4cbeca04b0daa34733c03ec20
samulimarkus <minjonlu@gmail.com>
2021-04-04 16:33:22 +0300

    Use Regular Expression Constraints on route file and make it cleaner

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,25 @@
 <?php
 
+use App\Http\Controllers\Carriers\DeleteCarrierController;
+use App\Http\Controllers\Carriers\ListCarriersController;
+use App\Http\Controllers\Carriers\ModeOfTransports\ListCarrierModeOfTransportsController;
+use App\Http\Controllers\Carriers\ModeOfTransports\ShowCarrierModeOfTransportCreationFormController;
+use App\Http\Controllers\Carriers\ModeOfTransports\ShowCarrierModeOfTransportEditFormController;
+use App\Http\Controllers\Carriers\ShowCarrierCreationFormController;
+use App\Http\Controllers\Carriers\ShowCarrierEditFormController;
+use App\Http\Controllers\Carriers\StoreCarrierController;
+use App\Http\Controllers\Carriers\UpdateCarrierController;
+use App\Http\Controllers\Orders\DeleteOrderController;
+use App\Http\Controllers\Orders\ListOrdersController;
+use App\Http\Controllers\Orders\ShowOrderCreationFormController;
+use App\Http\Controllers\Orders\ShowOrderEditFormController;
+use App\Http\Controllers\Orders\UpdateOrderController;
+use App\Http\Controllers\Vendors\DeleteVendorController;
+use App\Http\Controllers\Vendors\ListVendorsController;
+use App\Http\Controllers\Vendors\ShowVendorCreationFormController;
+use App\Http\Controllers\Vendors\ShowVendorEditFormController;
+use App\Http\Controllers\Vendors\StoreVendorController;
+use App\Http\Controllers\Vendors\UpdateVendorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,35 +33,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Vendors...
-Route::get('vendors', 'Vendors\ListVendorsController')->name('vendors.index');
-Route::get('vendors/create', 'Vendors\ShowVendorCreationFormController')->name('vendors.create');
-Route::get('vendors/{vendor}/edit', 'Vendors\ShowVendorEditFormController')->name('vendors.edit');
-Route::patch('vendors/{vendor}', 'Vendors\UpdateVendorController')->name('vendors.update');
-Route::post('vendors/', 'Vendors\StoreVendorController')->name('vendors.store');
-Route::delete('vendors/', 'Vendors\DeleteVendorController')->name('vendors.delete');
+Route::get('vendors', [ListVendorsController::class, '__invoke'])->name('vendors.index');
+Route::get('vendors/create', [ShowVendorCreationFormController::class, '__invoke'])->name('vendors.create');
+Route::get('vendors/{vendor}/edit', [ShowVendorEditFormController::class, '__invoke'])->name('vendors.edit')->whereNumber('vendor');
+Route::patch('vendors/{vendor}', [UpdateVendorController::class, '__invoke'])->name('vendors.update')->whereNumber('vendor');
+Route::post('vendors/', [StoreVendorController::class, '__invoke'])->name('vendors.store');
+Route::delete('vendors/', [DeleteVendorController::class, '__invoke'])->name('vendors.delete');
 
 // Carriers...
-Route::get('carriers', 'Carriers\ListCarriersController')->name('carriers.index');
-Route::get('carriers/create', 'Carriers\ShowCarrierCreationFormController')->name('carriers.create');
-Route::get('carriers/{carrier}/edit', 'Carriers\ShowCarrierEditFormController')->name('carriers.edit');
-Route::patch('carriers/{carrier}', 'Carriers\UpdateCarrierController')->name('carriers.update');
-Route::post('carriers/', 'Carriers\StoreCarrierController')->name('carriers.store');
-Route::delete('carriers/', 'Carriers\DeleteCarrierController')->name('carriers.delete');
+Route::get('carriers', [ListCarriersController::class, '__invoke'])->name('carriers.index');
+Route::get('carriers/create', [ShowCarrierCreationFormController::class, '__invoke'])->name('carriers.create');
+Route::get('carriers/{carrier}/edit', [ShowCarrierEditFormController::class, '__invoke'])->name('carriers.edit')->whereNumber('carrier');
+Route::patch('carriers/{carrier}', [UpdateCarrierController::class, '__invoke'])->name('carriers.update')->whereNumber('carrier');
+Route::post('carriers/', [StoreCarrierController::class, '__invoke'])->name('carriers.store');
+Route::delete('carriers/', [DeleteCarrierController::class, '__invoke'])->name('carriers.delete')->whereNumber('carrier');
 
 // Carrier mode of transports...
-Route::get('carriers/{carrier}/mode-of-transports', 'Carriers\ModeOfTransports\ListCarrierModeOfTransportsController')->name('carriers.mode-of-transports.index');
-Route::get('carriers/{carrier}/mode-of-transports/create', 'Carriers\ModeOfTransports\ShowCarrierModeOfTransportCreationFormController')->name('carriers.mode-of-transports.create');
-Route::get('carriers/{carrier}/mode-of-transports/edit/{modeOfTransport}', 'Carriers\ModeOfTransports\ShowCarrierModeOfTransportEditFormController')->name('carriers.mode-of-transports.edit');
+Route::get('carriers/{carrier}/mode-of-transports', [ListCarrierModeOfTransportsController::class, '__invoke'])->name('carriers.mode-of-transports.index')->whereNumber('carrier');
+Route::get('carriers/{carrier}/mode-of-transports/create', [ShowCarrierModeOfTransportCreationFormController::class, '__invoke'])->name('carriers.mode-of-transports.create')->whereNumber('carrier');
+Route::get('carriers/{carrier}/mode-of-transports/edit/{modeOfTransport}', [ShowCarrierModeOfTransportEditFormController::class, '__invoke'])->name('carriers.mode-of-transports.edit')->whereNumber('carrier')->whereNumber('modeOfTransport');
 
 // Orders...
-Route::get('orders', 'Orders\ListOrdersController')->name('orders.index');
-Route::get('orders/{order}/edit', 'Orders\ShowOrderEditFormController')->name('orders.edit');
-Route::patch('orders/{order}', 'Orders\UpdateOrderController')->name('orders.update');
-Route::get('orders/create', 'Orders\ShowOrderCreationFormController')->name('orders.create');
-Route::post('orders/', 'Orders\StoreOrderController')->name('orders.store');
-Route::delete('orders/', 'Orders\DeleteOrderController')->name('orders.delete');
+Route::get('orders', [ListOrdersController::class, '__invoke'])->name('orders.index');
+Route::get('orders/{order}/edit', [ShowOrderEditFormController::class, '__invoke'])->name('orders.edit')->whereNumber('order');
+Route::patch('orders/{order}', [UpdateOrderController::class, '__invoke'])->name('orders.update')->whereNumber('order');
+Route::get('orders/create', [ShowOrderCreationFormController::class, '__invoke'])->name('orders.create');
+Route::post('orders/', [StoreOrderController::class, '__invoke'])->name('orders.store');
+Route::delete('orders/', [DeleteOrderController::class, '__invoke'])->name('orders.delete');
