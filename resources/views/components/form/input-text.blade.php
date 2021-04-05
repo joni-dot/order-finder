5374eaa759b9080e04d630c4f2d42aebd9823022
samulimarkus <minjonlu@gmail.com>
2021-04-05 14:55:24 +0300

    Created Blade component for text inputs

---
+++ resources/views/components/form/input-text.blade.php
@@ -0,0 +1,5 @@
+<input 
+    class="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-700" 
+    type="text"
+    {{ $attributes }}
+>
--- resources/views/livewire/carriers/components/_form.blade.php
+++ resources/views/livewire/carriers/components/_form.blade.php
@@ -5,13 +5,11 @@
         </x-form.label>
     </div>
     <div>
-        <input 
-            class="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-700" 
+        <x-form.input-text
             id="name" 
             name="name"
-            type="text"
             wire:model="name"
-        >
+        />
     </div>
 </div>
 @error('name')
