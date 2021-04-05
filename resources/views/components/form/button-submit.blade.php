62364a295b4298e870b80fd14bd6cf74a56383e6
samulimarkus <minjonlu@gmail.com>
2021-04-05 17:41:04 +0300

    Blade component for form submit button

---
+++ resources/views/components/form/button-submit.blade.php
@@ -0,0 +1,6 @@
+<button 
+    class="shadow bg-purple-700 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" 
+    type="submit"
+>
+    Save
+</button>
--- resources/views/livewire/carriers/components/_form.blade.php
+++ resources/views/livewire/carriers/components/_form.blade.php
@@ -14,10 +14,5 @@
 </div>
 <x-form.input-error :name="'name'" :text="'Name'"/>
 <div class="text-left">
-    <button 
-        class="shadow bg-purple-700 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" 
-        type="submit"
-    >
-        Save
-    </button>
+    <x-form.button-submit/>
 </div>
