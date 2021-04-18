87a51fc0847cd757b7c0b53d2c6ba7d1842dcc22
samulimarkus <minjonlu@gmail.com>
2021-04-18 13:56:23 +0300

    Carrier mode of transport creation route fix

--- resources/views/carriers/mode-of-transports/index.blade.php
+++ resources/views/carriers/mode-of-transports/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     @include('carriers.components.sub-nav', ['carrier' => $carrier])
     <div class="text-right">
-        <x-anchors.buttons.create :href="route('carriers.mode-of-transports.create', $carrier->id)" :text="'New mode of transport'"/>
+        <x-anchors.buttons.create :href="route('carriers.mode-of-transports.create', $carrier->slug)" :text="'New mode of transport'"/>
     </div>
     <div class="my-4">
         <livewire:carriers.mode-of-transports.carrier-mode-of-transport-listing :carrier="$carrier"/>
