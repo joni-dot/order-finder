94db98008cb02c5d2ce7df1fea843ec7a66ee481
samulimarkus <minjonlu@gmail.com>
2021-04-18 16:50:42 +0300

    Build card for mode transports listing

--- resources/views/carriers/mode-of-transports/index.blade.php
+++ resources/views/carriers/mode-of-transports/index.blade.php
@@ -1,9 +1,16 @@
 <x-layout>
     @include('carriers.components.sub-nav', ['carrier' => $carrier])
-    <div class="text-right">
-        <x-anchors.buttons.create :href="route('carriers.mode-of-transports.create', $carrier->slug)" :text="'New mode of transport'"/>
-    </div>
-    <div class="my-4">
-        <livewire:carriers.mode-of-transports.carrier-mode-of-transport-listing :carrier="$carrier"/>
+    <div class="h-100 w-full flex bg-teal-lightest shadow">
+        <div class="bg-white rounded shadow w-full p-6">
+            <div class="w-full">
+                <h1 class="text-grey-darkest font-bold text-lg">Mode of transports</h1>
+                <div class="w-full mt-4 mb-4 text-right">
+                    <x-anchors.buttons.create :href="route('carriers.mode-of-transports.create', $carrier->slug)" :text="'New mode of transport'"/>
+                </div>  
+            </div>
+            <div>
+                <livewire:carriers.mode-of-transports.carrier-mode-of-transport-listing :carrier="$carrier"/>
+            </div>
+        </div>
     </div>
 </x-layout>
