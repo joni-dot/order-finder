d309286623f95be2e753cfc043a59a00a5143a58
samulimarkus <minjonlu@gmail.com>
2021-05-15 16:11:31 +0300

    Customize Laravel login page and show Livewire validation errors

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,46 +1,55 @@
-
-<form wire:submit.prevent="login">
-    @csrf
-    <!-- Email Address -->
-    <div>
-        <x-label for="email" :value="__('Email')" />
-        <x-input 
-            id="email" 
-            class="block mt-1 w-full" 
-            type="email" 
-            name="email" 
-            :value="old('email')" 
-            wire:model="email"/>
+<div class="flex p-20">
+    <div class="flex-1"></div>
+    <div class="flex-1 bg-indigo-300 p-5 rounded">
+        <form wire:submit.prevent="login">
+            <div class="mb-4">
+                <label class="font-bold block mb-1" for="email">Email</label>
+                <input
+                    id="email" 
+                    class="block w-full rounded border-solid border-2 border-white" 
+                    type="email" 
+                    name="email" 
+                    :value="old('email')" 
+                    wire:model="email"/>
+                @error('email')
+                    <div class="font-bold text-white mt-2 bg-red-500 p-1 pl-3 pr-3 rounded">{{  $message  }}</div>
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label class="font-bold block mb-1" for="password">Password</label>
+                <input 
+                    id="password" 
+                    class="block w-full rounded border-solid border-2 border-white"
+                    type="password"
+                    name="password"
+                    wire:model="password"
+                />
+                @error('password')
+                    <div class="font-bold text-white mt-2 bg-red-500 p-1 pl-3 pr-3 rounded">{{  $message  }}</div>
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label class="font-bold" for="remember_me">
+                    <input 
+                        id="remember_me" 
+                        type="checkbox" 
+                        class="form-checkbox h-5 w-5 text-gray-600 mr-1" 
+                        name="remember"
+                    >
+                    <span>Remember me</span>
+                </label>
+            </div>
+            <div>
+                <button class="float-left font-bold bg-white py-1 pb-1 pr-4 pl-4 rounded">
+                    Log in
+                </button>
+                @if (Route::has('password.request'))
+                    <a class="float-right font-bold" href="{{ route('password.request') }}">
+                        Forgot your password?
+                    </a>
+                @endif
+            </div>
+        </form>
     </div>
-    <!-- Password -->
-    <div class="mt-4">
-        <x-label for="password" :value="__('Password')" />
-        <x-input id="password" class="block mt-1 w-full"
-            type="password"
-            name="password"
-            wire:model="password"
-        />
-    </div>
-    <!-- Remember Me -->
-    <div class="block mt-4">
-        <label for="remember_me" class="inline-flex items-center">
-            <input 
-                id="remember_me" 
-                type="checkbox" 
-                class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
-                name="remember"
-            >
-            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-        </label>
-    </div>
-    <div class="flex items-center justify-end mt-4">
-        @if (Route::has('password.request'))
-            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
-            </a>
-        @endif
-        <x-button class="ml-3">
-            {{ __('Log in') }}
-        </x-button>
-    </div>
-</form>
+    <div class="flex-1"></div>
+</div>
