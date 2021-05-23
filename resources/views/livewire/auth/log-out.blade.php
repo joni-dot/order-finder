b6ecc3693cf39167d3380f4eba96562bf859a056
samulimarkus <minjonlu@gmail.com>
2021-05-23 15:20:09 +0300

    Add loading states to log out livewire component

--- resources/views/livewire/auth/log-out.blade.php
+++ resources/views/livewire/auth/log-out.blade.php
@@ -1,1 +1,20 @@
-<a href="#" wire:click="logOut" class="text-white text-m font-bold">Log out</a>
+<span>
+    <a 
+        wire:click="logOut" 
+        wire:loading.remove 
+        id="log-out-anchor" 
+        href="#" 
+        class="text-white text-m font-bold"
+    >
+        @svg('fast-forward', 'fill-current w-4 h-4 mr-1 ml-0 inline')
+    </a>
+    <a 
+        wire:loading 
+        wire:target="logOut"
+        id="log-out-loading-icon" 
+        href="" 
+        class="text-white text-m font-bold"
+    >
+        @svg('refresh', 'animate-spin fill-current w-4 h-4 mr-1 ml-0 inline')
+    </a>
+</span>
